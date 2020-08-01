fe224380196967dc96b13a4896234a3c784e7e36
miqueias-araujo <eentao@gmail.com>
2020-08-01 14:16:39 -0400

    mensagem primeira

---
+++ src/easyJDBCConnection/jdbcconnection.java
@@ -0,0 +1,32 @@
+package easyJDBCConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class jdbcconnection {
+
+	public static void main(String[] args) {
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.1.28:1521/orcl", "HR", "oracle");
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from EMPLOYEES");
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + " - " + rs.getString(2) + " - - " + rs.getString(3));
+			}
+			rs.close();
+			st.close();
+			con.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			System.out.println("fin da lista");
+		}
+
+
+	}
+
+}
